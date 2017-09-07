26b68ceb8ec5ccad999a436c7d594d1df3398f04
Nao Pross <naopross@thearcway.org>
2017-09-07 14:27:14 +0200

    z80_test cleanup and makefile corrections

--- sw/z80_test/main.c
+++ sw/z80_test/main.c
@@ -2,6 +2,10 @@
 
 void main(void)
 {
-    usart_init(USART_BAUDRATE_9600, USART_PARITY_EVEN, USART_STOP_BITS_1);
-    usart_write("Hello, World!", 13);
+    usart_init(USART_BAUDRATE_1200, USART_PARITY_EVEN, USART_STOP_BITS_1);
+    usart_write("Hello, World!\n", 13);
+
+    while(1) {
+        usart_write("done\n", 5);
+    }
 }
